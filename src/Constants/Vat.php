4471491799f9bc11f3a9a624b593d2fb9eba5fd5
Mikael Agabalyants <njxqlus@gmail.com>
2020-03-16 17:31:13 +0300

    update(Vat.php): add 10/110 & 20/120 vat values

--- src/Constants/Vat.php
+++ src/Constants/Vat.php
@@ -10,4 +10,6 @@ class Vat
     const vat0  = 'vat0'; // НДС 0%
     const vat10 = 'vat10';// НДС 10%
     const vat20 = 'vat20'; // НДС 20%
+    const vat110 = 'vat110'; // 10/110
+    const vat120 = 'vat120'; // 20/120
 }
